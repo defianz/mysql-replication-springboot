f8d30ad8592d46c5b03abf6c03ffe91d2fa8ce4a
Defian <iautm91@gmail.com>
2021-09-14 20:51:41 +0900

    Mysql Replication 구성

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mysql replication with SpringBoot
+##  Mysql replication 을 통해 Master - Slave 구성하여, SpringBoot의 transaction 요청을 분개하여 처리
+
+- Docker, mysql, SpringBoot
+
+- 참고 : https://velog.io/@max9106/DB-Spring-Replication
---
+++ mysql-replication/first_query.sql
@@ -0,0 +1,19 @@
+# https://velog.io/@max9106/DB-Spring-Replication 참고
+
+# master 쪽 계정 생성 - 권한 부여
+CREATE USER 'defian'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'defian'@'%';
+flush privileges;
+quit;
+
+# master status 확인
+show master status;
+
+# slave server - 마스터 설정
+CHANGE MASTER TO MASTER_HOST='172.22.0.2', MASTER_PORT=3306, MASTER_USER='root', MASTER_PASSWORD='password', MASTER_LOG_FILE='binlog.000002',MASTER_LOG_POS=47548;
+
+# slave 실행
+START SLAVE;
+
+# slave 연결 확인
+SHOW SLAVE STATUS\G;
